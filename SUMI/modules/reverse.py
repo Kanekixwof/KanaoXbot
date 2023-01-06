5d6cbc43c314ff09b2eaf2b692e69feeeb0b2dcf
Infamous-Hydra <116187035+Infamous-Hydra@users.noreply.github.com>
2023-01-06 15:22:26 +0600

    Update reverse.py

--- SUMI/modules/reverse.py
+++ SUMI/modules/reverse.py
@@ -1,34 +1,44 @@
+#Modules from AsukaRobot
+#credit goes to the original writer of this module
+
 import os
 import re
 import urllib
 import urllib.parse
 import urllib.request
-from urllib.error import HTTPError, URLError
+from urllib.error import URLError, HTTPError
 
 import requests
 from bs4 import BeautifulSoup
-from telegram import InputMediaPhoto, TelegramError, Update
-from telegram.ext import CallbackContext
+from telegram import InputMediaPhoto, TelegramError
 
 from SUMI import dispatcher
 from SUMI.modules.disable import DisableAbleCommandHandler
+from SUMI.modules.helper_funcs.alternate import typing_action
 
 opener = urllib.request.build_opener()
-useragent = "Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36"
+useragent = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.38 "
+    "Safari/537.36 "
+)
+# useragent = 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko)
+# Chrome/52.0.2743.98 Mobile Safari/537.36'
 opener.addheaders = [("User-agent", useragent)]
 
 
-def reverse(update: Update, context: CallbackContext):
+@typing_action
+def reverse(update, context):
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
 
     msg = update.effective_message
     chat_id = update.effective_chat.id
-    bot, args = context.bot, context.args
     rtmid = msg.message_id
+    args = context.args
     imagename = "okgoogle.png"
 
-    if reply := msg.reply_to_message:
+    reply = msg.reply_to_message
+    if reply:
         if reply.sticker:
             file_id = reply.sticker.file_id
         elif reply.photo:
@@ -36,15 +46,15 @@ def reverse(update: Update, context: CallbackContext):
         elif reply.document:
             file_id = reply.document.file_id
         else:
-            msg.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴏʀ sᴛɪᴄᴋᴇʀ ᴛᴏ ʟᴏᴏᴋᴜᴘ.")
+            msg.reply_text("Reply to an image or sticker to lookup.")
             return
-        image_file = bot.get_file(file_id)
+        image_file = context.bot.get_file(file_id)
         image_file.download(imagename)
         if args:
             txt = args[0]
             try:
                 lim = int(txt)
-            except:
+            except BaseException:
                 lim = 2
         else:
             lim = 2
@@ -54,34 +64,36 @@ def reverse(update: Update, context: CallbackContext):
             img_link = splatargs[1]
             try:
                 lim = int(splatargs[2])
-            except:
+            except BaseException:
                 lim = 2
         elif len(splatargs) == 2:
             img_link = splatargs[1]
             lim = 2
         else:
-            msg.reply_text("/reverse <link> <amount ᴏғ ɪᴍᴀɢᴇs ᴛᴏ ʀᴇᴛᴜʀɴ.>")
+            msg.reply_text("/reverse <link> <amount of images to return.>")
             return
         try:
             urllib.request.urlretrieve(img_link, imagename)
         except HTTPError as HE:
-            if HE.reason == "Not Found":
-                msg.reply_text("ɪᴍᴀɢᴇ ɴᴏᴛ ғᴏᴜɴᴅ.")
-                return
             if HE.reason == "Forbidden":
                 msg.reply_text(
-                    "ᴄᴏᴜʟᴅɴ'ᴛ ᴀᴄᴄᴇss ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇᴅ ʟɪɴᴋ, ᴛʜᴇ ᴡᴇʙsɪᴛᴇ ᴍɪɢʜᴛ ʜᴀᴠᴇ ʙʟᴏᴄᴋᴇᴅ ᴀᴄᴄᴇssɪɴɢ ᴛᴏ ᴛʜᴇ website ʙʏ ʙᴏᴛ ᴏʀ ᴛʜᴇ ᴡᴇʙsɪᴛᴇ ᴅᴏᴇs ɴᴏᴛ ᴇxɪsᴛᴇᴅ."
+                    "Couldn't access the provided link, The website might have blocked accessing to the website by "
+                    "bot or the website does not existed. "
                 )
+                return
+            if HE.reason == "Not Found":
+                msg.reply_text("Image not found.")
                 return
         except URLError as UE:
             msg.reply_text(f"{UE.reason}")
             return
         except ValueError as VE:
-            msg.reply_text(f"{VE}\nᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ᴜsɪɴɢ ʜᴛᴛᴘ ᴏʀ ʜᴛᴛᴘs ᴘʀᴏᴛᴏᴄᴏʟ.")
+            msg.reply_text(f"{VE}\nPlease try again using http or https protocol.")
             return
     else:
         msg.reply_markdown(
-            "ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ, ᴏʀ ᴀɴ ɪᴍᴀɢᴇ to sᴇᴀʀᴄʜ it!\nᴅᴏ ʏᴏᴜ ᴋɴᴏᴡ ᴛʜᴀᴛ ʏᴏᴜ ᴄᴀɴ sᴇᴀʀᴄʜ ᴀɴ ɪᴍᴀɢᴇ ᴡɪᴛʜ a ʟɪɴᴋ ᴛᴏᴏ? `/reverse [ᴘɪᴄᴛᴜʀᴇʟɪɴᴋ] <ᴀᴍᴏᴜɴᴛ>`."
+            "Please reply to a sticker, or an image to search it!\nDo you know that you can search an image with a "
+            "link too? `/reverse [picturelink] <amount>`. "
         )
         return
 
@@ -95,21 +107,19 @@ def reverse(update: Update, context: CallbackContext):
         fetchUrl = response.headers["Location"]
 
         if response != 400:
-            xx = bot.send_message(
+            xx = context.bot.send_message(
                 chat_id,
-                "ɪᴍᴀɢᴇ ᴡᴀs sᴜᴄᴄᴇssғᴜʟʟʏ ᴜᴘʟᴏᴀᴅᴇᴅ ᴛᴏ ɢᴏᴏɢʟᴇ." "\n ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ʙᴀʙʏ...",
+                "Image was successfully uploaded to Google.",
                 reply_to_message_id=rtmid,
             )
         else:
-            xx = bot.send_message(
-                chat_id,
-                "ɢᴏᴏɢʟᴇ ᴛᴏʟᴅ ᴍᴇ ᴛᴏ ғᴜᴄᴋ ʏᴏᴜ ᴛʜɪs ɢᴀʏ",
-                reply_to_message_id=rtmid,
+            xx = context.bot.send_message(
+                chat_id, "Google told me to go away.", reply_to_message_id=rtmid
             )
             return
 
         os.remove(imagename)
-        match = ParseSauce(f"{fetchUrl}&hl=en")
+        match = ParseSauce(fetchUrl + "&hl=en")
         guess = match["best_guess"]
         if match["override"] and match["override"] != "":
             imgspage = match["override"]
@@ -118,18 +128,18 @@ def reverse(update: Update, context: CallbackContext):
 
         if guess and imgspage:
             xx.edit_text(
-                f"[{guess}]({fetchUrl})\nWaito...",
+                f"[{guess}]({fetchUrl})\nLooking for images...",
                 parse_mode="Markdown",
                 disable_web_page_preview=True,
             )
         else:
-            xx.edit_text("ᴄᴏᴜʟᴅɴ'ᴛ ғɪɴᴅ ᴀɴʏᴛʜɪɴɢ.")
+            xx.edit_text("Couldn't find anything.")
             return
 
         images = scam(imgspage, lim)
         if len(images) == 0:
             xx.edit_text(
-                f"[{guess}]({fetchUrl})[.]({imgspage})",
+                f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})",
                 parse_mode="Markdown",
                 disable_web_page_preview=True,
             )
@@ -140,9 +150,11 @@ def reverse(update: Update, context: CallbackContext):
             lmao = InputMediaPhoto(media=str(link))
             imglinks.append(lmao)
 
-        bot.send_media_group(chat_id=chat_id, media=imglinks, reply_to_message_id=rtmid)
+        context.bot.send_media_group(
+            chat_id=chat_id, media=imglinks, reply_to_message_id=rtmid
+        )
         xx.edit_text(
-            f"[{guess}]({fetchUrl})[.]({imgspage})",
+            f"[{guess}]({fetchUrl})\n\n[Visually similar images]({imgspage})",
             parse_mode="Markdown",
             disable_web_page_preview=True,
         )
@@ -153,8 +165,6 @@ def reverse(update: Update, context: CallbackContext):
 
 
 def ParseSauce(googleurl):
-    """Parse/Scrape the HTML code for the info we want."""
-
     source = opener.open(googleurl).read()
     soup = BeautifulSoup(source, "html.parser")
 
@@ -164,7 +174,7 @@ def ParseSauce(googleurl):
         for bess in soup.findAll("a", {"class": "PBorbe"}):
             url = "https://www.google.com" + bess.get("href")
             results["override"] = url
-    except:
+    except BaseException:
         pass
 
     for similar_image in soup.findAll("input", {"class": "gLFyf"}):
@@ -203,9 +213,10 @@ def scam(imgspage, lim):
 
 
 REVERSE_HANDLER = DisableAbleCommandHandler(
-    ["grs", "reverse", "pp"], reverse, pass_args=True, run_async=True
+    ["reverse", "grs", "p", "pp", "lens"],
+    reverse,
+    pass_args=True,
+    run_async=True,
 )
 
 dispatcher.add_handler(REVERSE_HANDLER)
-
-__mod_name__ = "𝚁ᴇᴠᴇʀsᴇ"

42551e0f6ebec6fbddd97070c0bfef79aba49ec3
Infamous-Hydra <116187035+Infamous-Hydra@users.noreply.github.com>
2023-01-07 12:17:42 +0600

    Update paste.py

--- SUMI/modules/paste.py
+++ SUMI/modules/paste.py
@@ -33,7 +33,7 @@ async def isPreviewUp(preview: str) -> bool:
     return False
 
 
-@pgram.on_message(filters.command("paste"))
+@pgram.on_message(filters.command("paste") & ~filters.edited)
 @capture_err
 async def paste_func(_, message):
     if not message.reply_to_message:
